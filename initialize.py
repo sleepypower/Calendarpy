070fbabf0119c8231261bbd1186950d328de23ec
David <daalmega@gmail.com>
2021-06-03 23:54:32 -0500

    Fixed bugs and added background Image

--- initialize.py
+++ initialize.py
@@ -9,10 +9,9 @@ def set_pygame_and_screen(ScreenHeight = 600, ScreenWidth = 600):
     Sets the screen
     """
 
-
     #SCREEN setup
     SCREEN_COLOR = (40, 40, 40)
 
-    SCREEN = pygame.display.set_mode((ScreenWidth, ScreenHeight)) #Create Screen
+    SCREEN = pygame.display.set_mode((ScreenHeight, ScreenWidth)) #Create Screen
     SCREEN.fill(SCREEN_COLOR)
     return SCREEN
--- main.py
+++ main.py
@@ -6,32 +6,38 @@ import pygame
 import initialize
 import time
 
-pygame.init() #preparar el módulo de juego
+
+# initialize game engine
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 1200
+
+clock_tick_rate = 20
+
+# Open a window
+
+screen = initialize.set_pygame_and_screen(SCREEN_HEIGHT, SCREEN_WIDTH)
+
+# Set title to the window
+pygame.display.set_caption("CALENDARPY")
+
 clock = pygame.time.Clock()
+BACKGROUND_IMAGE = pygame.image.load("background_image.jpg")
+BACKGROUND_IMAGE = pygame.transform.smoothscale(
+    BACKGROUND_IMAGE, (SCREEN_HEIGHT, SCREEN_WIDTH))
 
-#Crear la superficie y ventana que la contiene
-SCREEN_HEIGHT = 600
-SCREEN_WIDTH = 1200
-SCREEN_COLOR = (40, 40, 40)
+running = True
 
-SCREEN = initialize.set_pygame_and_screen(SCREEN_HEIGHT, SCREEN_WIDTH)
+while running:
 
-last_move = pygame.time.get_ticks()
-
-frame = 0
-
-while True:
-
-    events = pygame.event.get()
-
-    for event in events:
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            pass
-        
-    SCREEN.fill(SCREEN_COLOR)
+            running = False
 
+    screen.blit(BACKGROUND_IMAGE, [0, 0])
 
     pygame.display.flip()
-    clock.tick(1)
+    clock.tick(clock_tick_rate)
+
+pygame.quit()
