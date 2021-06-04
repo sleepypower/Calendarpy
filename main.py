67c7c962bb8f44a6947c40164b39b30d152348d1
David <daalmega@gmail.com>
2021-06-04 18:44:03 -0500

    Centered calendar surface

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ BACKGROUND_IMAGE = pygame.transform.smoothscale(
     BACKGROUND_IMAGE, (globals.SCREEN_HEIGHT, globals.SCREEN_WIDTH))
 
 running = True
-
-s = pygame.Surface((1000, 750))  # the size of your rect
+calendar_surface_size = (int(globals.SCREEN_HEIGHT * 0.85),
+                         int(globals.SCREEN_WIDTH * 0.85))
+s = pygame.Surface(calendar_surface_size)
 s.set_alpha(128)                # alpha level
 s.fill((255, 255, 255))           # this fills the entire surface
 
@@ -40,7 +41,8 @@ while running:
             running = False
 
     BACKGROUND_SCREEN.blit(BACKGROUND_IMAGE, [0, 0])
-    BACKGROUND_SCREEN.blit(s, (0, 0))
+    BACKGROUND_SCREEN.blit(
+        s, ((globals.SCREEN_HEIGHT - calendar_surface_size[0])/2, (globals.SCREEN_WIDTH - calendar_surface_size[1])/2))
     pygame.display.flip()
     clock.tick(clock_tick_rate)
 
