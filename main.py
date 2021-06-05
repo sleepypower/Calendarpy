589903f2567a00688334ce1fb764d71122125c05
David <daalmega@gmail.com>
2021-06-05 09:43:00 -0500

    changed main

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ import globals
 # initialize game engine
 pygame.init()
 
+# Font
+pygame.font.init()
+
 
 clock_tick_rate = 60
 
 # Open a window
-
 BACKGROUND_SCREEN = initialize.set_pygame_and_screen(
     globals.SCREEN_HEIGHT, globals.SCREEN_WIDTH)
 
@@ -28,9 +30,8 @@ BACKGROUND_IMAGE = pygame.transform.smoothscale(
     BACKGROUND_IMAGE, (globals.SCREEN_HEIGHT, globals.SCREEN_WIDTH))
 
 running = True
-calendar_surface_size = (int(globals.SCREEN_HEIGHT * 0.85),
-                         int(globals.SCREEN_WIDTH * 0.85))
-s = pygame.Surface(calendar_surface_size)
+
+s = pygame.Surface(globals.CALENDAR_SURFACE_SIZE)
 s.set_alpha(128)                # alpha level
 s.fill((255, 255, 255))           # this fills the entire surface
 
@@ -42,7 +43,9 @@ while running:
 
     BACKGROUND_SCREEN.blit(BACKGROUND_IMAGE, [0, 0])
     BACKGROUND_SCREEN.blit(
-        s, ((globals.SCREEN_HEIGHT - calendar_surface_size[0])/2, (globals.SCREEN_WIDTH - calendar_surface_size[1])/2))
+        s, ((globals.SCREEN_HEIGHT - globals.CALENDAR_SURFACE_SIZE[0])/2,
+            (globals.SCREEN_WIDTH - globals.CALENDAR_SURFACE_SIZE[1])/2))
+
     pygame.display.flip()
     clock.tick(clock_tick_rate)
 
