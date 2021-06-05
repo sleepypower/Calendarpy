f768f9b770b34cba2eb4ce8b1742639a15fdef6c
David <daalmega@gmail.com>
2021-06-05 09:41:53 -0500

    Added classBlock and Calendar

--- classBlock.py
+++ classBlock.py
@@ -1,6 +1,21 @@
 import pygame
+import globals
 
-class ClassBlock:
+pygame.font.init()
+
+
+class Calendar:
 
     def __init__(self) -> None:
         pass
+
+class ClassBlock:
+
+    def __init__(self, class_name: str, starting_time: int, ending_time: int) -> None:
+        delta_time = ending_time - starting_time
+        s = pygame.Surface((globals.CALENDAR_SURFACE_SIZE,
+                            globals.CALENDAR_SURFACE_SIZE))
+        s.set_alpha(128)
+        s.fill((255, 255, 255))
+
+    
